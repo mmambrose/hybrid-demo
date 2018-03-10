7eba4270bcd267fc76fd11a63928e2f15d8dbeec
Maggie Ambrose <mambrose@pivotal.io>
2018-03-10 12:16:02 -0600

    ordermgmt publishes message to topic

---
+++ order-mgmt-service/README.md
@@ -0,0 +1,4 @@
+⋅⋅1. `./mvnw compile`
+2. `./mvnw clean package`
+3. `cf create-service azure-servicebus standard order-mgmt-bus`
+4. `cf push`
--- order-mgmt-service/src/main/java/io/pivotal/ecosystem/ordermgmtservice/OrderMgmtServiceApplication.java
+++ order-mgmt-service/src/main/java/io/pivotal/ecosystem/ordermgmtservice/OrderMgmtServiceApplication.java
@@ -1,5 +1,6 @@
 package io.pivotal.ecosystem.ordermgmtservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,4 +10,5 @@ public class OrderMgmtServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderMgmtServiceApplication.class, args);
 	}
+
 }
