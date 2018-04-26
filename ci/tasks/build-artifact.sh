fe4f07fa27d14b09cd30742d188fb184220ecedf
Maggie Ambrose <mambrose@pivotal.io>
2018-04-26 13:49:29 -0500

    testing ci for web-store

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -3,8 +3,8 @@
 set -e -x
 
 
-pushd git-repo/
+pushd git-repo/web-store
   ./mvnw clean package
 popd
 
-cp git-repo/target/concourse-app-0.0.1-SNAPSHOT.jar   build-output/.
+cp git-repo/web-store/target/concourse-app-0.0.1-SNAPSHOT.jar   build-output/.
